748f952c742c0ee9b2b4ee16acb862ba21a62d9f
Danny Perez <xdanny28x@gmail.com>
2022-08-22 14:46:03 -0700

    complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let myArray = 0;
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') {
+        return 'ERROR';
+    }
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
+    if (a > b) {
+        [a, b] = [b, a];
+    }
+    while (a < b + 1) {
+        myArray += a;
+        a++;
+    }
+    return myArray;
 };
 
 // Do not edit below this line
