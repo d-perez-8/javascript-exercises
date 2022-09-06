f74290831b125e4a5b2144ab619146643c00ce68
Danny Perez <xdanny28x@gmail.com>
2022-09-06 09:28:36 -0700

    complete eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+  let answer = 0;
+  for (let nums in myArray) {
+    answer += myArray[nums];
+  }
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+  let product = 1;
+  for (let nums in myArray) {
+    product *= myArray[nums];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let newNum = num;
+	if (num < 0) {
+    return -1;
+  } else if (num == 0 || num == 1) {
+    return 1;
+  }
+  while (num > 1) {
+    num--;
+    newNum *= num;
+  }
+  return newNum
 };
 
 // Do not edit below this line
