5d08bf23004ca21be829792a4efbfdc6aab9d957
Danny Perez <xdanny28x@gmail.com>
2022-09-07 11:28:27 -0700

    complete tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (parseInt(position) > 0) {
+        let myNum = 1;
+        let lastNum = 0;
 
+        for (let index = 1; index < position; index++) {
+            myNum = myNum + lastNum;
+            lastNum = myNum - lastNum;
+        }
+
+        return myNum;
+    } else {
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
